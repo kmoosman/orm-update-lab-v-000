b72348af591ddb0b36cb1427eec74060cc82cdbf
Katie Moosman <moosman.katie@gmail.com>
2019-02-06 17:05:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,9 @@ class Student
       name TEXT,
       grade INTEGER)
     SQL
+
+    DB[:conn].execute(sql)
+
   end
 
   # Remember, you can access your database connection anywhere in this class
