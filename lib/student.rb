b90745ae41a62a3e6009240dc5812ed8b14da2b6
Katie Moosman <moosman.katie@gmail.com>
2019-02-06 17:30:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
      name = row[1]
      grade = row[2]
      student = self.new(name, grade)
-     student.id = row[0]
+     @id = row[0]
     #  student.id = id
      student
    end
