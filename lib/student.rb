11a47ef51fe60fc4df6d4498c37615fbc4340652
Katie Moosman <moosman.katie@gmail.com>
2019-02-06 17:25:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,9 +58,9 @@ class Student
 
    def self.new_from_db(row)
      student = Student.new(self.name, self.grade)
-     student.id = row[0]
+     student.id = row[0],
      student.name = row[1],
-     student.grade = row[2],
+     student.grade = row[2]
 
      student
    end
