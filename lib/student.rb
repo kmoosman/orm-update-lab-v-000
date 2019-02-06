300b258614c427dd91b1122de491bb9eeec0c418
Katie Moosman <moosman.katie@gmail.com>
2019-02-06 17:33:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,15 +56,7 @@ class Student
      student
    end
 
-  #  def self.new_from_db(row)
-  #    id = row[0]
-  #    name = row[1]
-  #    grade = row[2]
-  #    student = self.new(name, grade)
-  #    @id = row[0]
-  #   #  student.id = id
-  #    student
-  #  end
+
   def self.new_from_db(row)
     # create a new Student object given a row from the database
     student = self.new(row[1], row[2], row[0])
