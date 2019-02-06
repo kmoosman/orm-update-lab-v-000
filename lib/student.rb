7feac5fd855af146621cbdc1cf7f9a2c54948da2
Katie Moosman <moosman.katie@gmail.com>
2019-02-06 17:19:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
   #    student
   #  end
 
-   def self.create(name:, grade:)
+   def self.create(name, grade)
      student = Student.new(name, grade)
      student.save
      student
